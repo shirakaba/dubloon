24e6d42b9e7e2ec5f1767308392593d1a73e53fe
shirakaba <14055146+shirakaba@users.noreply.github.com>
2025-01-08 21:30:41 +0900

    restore missing brace in Android native code

--- packages/doubloon/android/src/main/java/uk/co/birchlabs/doubloon/DoubloonModule.kt
+++ packages/doubloon/android/src/main/java/uk/co/birchlabs/doubloon/DoubloonModule.kt
@@ -17,4 +17,5 @@ class DoubloonModule : Module() {
     // https://github.com/expo/expo/blob/034d4ff1c7b5fff88358a2a5cd4fa9f24baee570/packages/expo-device/android/src/main/java/expo/modules/device/DeviceModule.kt#L132-L133
     private val isRunningOnEmulator: Boolean
       get() = EmulatorUtilities.isRunningOnEmulator()
+  }
 }
