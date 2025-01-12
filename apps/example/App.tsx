8fa6e66ebd1e3e14ad920b8f0cc122f3a2d38493
shirakaba <14055146+shirakaba@users.noreply.github.com>
2025-01-12 12:04:23 +0900

    handle status bar on Android
    The status bar defaults to translucent, yet the default Android WebView doesn't populate safe-area-inset-*, so we have no way to compensate for it. The best we can do is make the status bar opaque.

--- apps/example/App.tsx
+++ apps/example/App.tsx
@@ -1,11 +1,16 @@
 import { connectionProps } from "dubloon";
+import { StatusBar } from "expo-status-bar";
+import * as React from "react";
 import WebView from "react-native-webview";
 
 export default function App() {
   return (
-    <WebView
-      webviewDebuggingEnabled={__DEV__}
-      {...connectionProps({ port: 5173 })}
-    />
+    <>
+      <WebView
+        webviewDebuggingEnabled={__DEV__}
+        {...connectionProps({ port: 5173 })}
+      />
+      <StatusBar backgroundColor="purple" translucent={false} />
+    </>
   );
 }
